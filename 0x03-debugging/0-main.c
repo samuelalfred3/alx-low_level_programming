b7ae609fc3801cdf3c17176894d030731e360352
samuelalfred3 <samuelalfrednjguna@gmail.com>
2023-08-17 23:55:00 +0300

    0-main.c

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,4 +1,4 @@
-#include "main.h"
+#include "main."
 
 /**
  * main - tests function that prints if integer is positive or negative
@@ -11,5 +11,6 @@ int main(void)
 
 	i = 0;
 	positive_or_negative(i);
+
 	return (0);
 }
--- 0x03-debugging/README.md
+++ 0x03-debugging/README.md
@@ -1,1 +1,1 @@
-0x03. C - Debugging
+This is a project on Debugging, and I am just learning to debug
